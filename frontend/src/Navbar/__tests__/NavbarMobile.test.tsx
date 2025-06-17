aa8fac92fb0e2489c59da9e5fdd788d38ef0d9e5
Matt Decker <mdeckerr@Matts-MBP.lan>
2025-06-16 20:57:59 -0500

    [#49] Feature: Navbar Atlas Logo Should Link to Homepage

--- frontend/src/Navbar/__tests__/NavbarMobile.test.tsx
+++ frontend/src/Navbar/__tests__/NavbarMobile.test.tsx
@@ -1,12 +1,30 @@
 import { render, screen } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import Navbar from '../Navbar';
 import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+
+const mockNavigate = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: () => mockNavigate,
+  };
+});
 
 describe('Navbar', () => {
-  const renderNavbar = () => render(<Navbar />);
+  const renderMobileNavbar = () => {
+    render(
+      <BrowserRouter>
+        <Navbar />;
+      </BrowserRouter>,
+    );
+  };
 
   beforeEach(() => {
+    mockNavigate.mockClear();
     Object.defineProperty(window, 'innerWidth', {
       writable: true,
       configurable: true,
@@ -22,7 +40,7 @@ describe('Navbar', () => {
   });
 
   it('mobile: should contain a site logo and company name  ', () => {
-    renderNavbar();
+    renderMobileNavbar();
 
     const siteName = screen.getByLabelText('atlas-site-name-mobile');
     const siteLogo = screen.getByLabelText('atlas-logo-mobile');
@@ -31,10 +49,32 @@ describe('Navbar', () => {
     expect(siteLogo).toBeVisible();
   });
 
+  it('mobile: should navigate to home if Atlas text is clicked', async () => {
+    renderMobileNavbar();
+
+    const siteName = screen.getByLabelText('atlas-site-name-mobile');
+
+    await userEvent.click(siteName);
+
+    expect(mockNavigate).toHaveBeenCalledWith('/');
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+  });
+
+  it('mobile: should navigate to home if Atlas Logo is clicked', async () => {
+    renderMobileNavbar();
+
+    const siteLogo = screen.getByLabelText('atlas-logo-mobile');
+
+    await userEvent.click(siteLogo);
+
+    expect(mockNavigate).toHaveBeenCalledWith('/');
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+  });
+
   it.each(['News', 'Matches'])(
     'mobile: should display hamburger menu, %s menu item when hamburger menu is clicked, and hide menu when user clicks away ',
     async (menuItem) => {
-      renderNavbar();
+      renderMobileNavbar();
 
       const mobileHamburgerNavigationMenu = screen.getByLabelText('navigation-links');
       expect(mobileHamburgerNavigationMenu).toBeVisible();
@@ -56,7 +96,7 @@ describe('Navbar', () => {
   it.each(['Profile', 'Logout'])(
     'mobile: should display profile icon, %s menu item when profile icon is clicked, and hide menu when user clicks away ',
     async (menuItem) => {
-      renderNavbar();
+      renderMobileNavbar();
 
       const profileIconMobile = screen.getByLabelText('Open Profile Settings');
       expect(profileIconMobile).toBeVisible();
